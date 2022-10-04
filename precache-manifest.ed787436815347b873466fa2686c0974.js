440068133563ac1ae504e62a324dcd17d2b27d0d
uhilario <uch2007@gmail.com>
2022-10-04 22:08:41 +0300

    Updates

---
+++ precache-manifest.ed787436815347b873466fa2686c0974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "069169e94c319edec14b",
+    "url": "/monsters-rolodex/static/css/main.84a58f9d.chunk.css"
+  },
+  {
+    "revision": "069169e94c319edec14b",
+    "url": "/monsters-rolodex/static/js/main.9f25dced.chunk.js"
+  },
+  {
+    "revision": "6c2d0b915730a523e189",
+    "url": "/monsters-rolodex/static/js/runtime~main.ade8ad79.js"
+  },
+  {
+    "revision": "5b1e7fa6d966527d9cbd",
+    "url": "/monsters-rolodex/static/js/2.d021738e.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/monsters-rolodex/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "9f3f2856b9f5f3b91059f66ada1b0880",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
